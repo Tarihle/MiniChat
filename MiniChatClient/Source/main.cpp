84f760cf7717fcfee97a895637de4489d44494f1
Louis Dupont <158799532+Tarihle@users.noreply.github.com>
2024-09-20 11:02:03 -0400

    Add console input handling in client project

--- MiniChatClient/Source/main.cpp
+++ MiniChatClient/Source/main.cpp
@@ -2,6 +2,113 @@
 #include <Network/Network.h>
 #include <stdio.h> /* printf */
 
+////////////////////////////////////////////////////////Readconsole
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#ifdef _UNICODE
+#undef _UNICODE
+#endif
+
+#ifdef UNICODE
+#undef UNICODE
+#endif
+
+HANDLE hStdin;
+DWORD fdwSaveOldMode;
+CHAR charbuf[256];
+short bufidx = 0;
+
+VOID ErrorExit(LPSTR lpszMessage)
+{
+    fprintf(stderr, "%s\n", lpszMessage);
+
+    // Restore input mode on exit.
+
+    SetConsoleMode(hStdin, fdwSaveOldMode);
+
+    ExitProcess(0);
+}
+
+VOID KeyEventProc(KEY_EVENT_RECORD ker, Net::Socket client)
+{
+    //printf("Key event: ");
+
+    if (!ker.bKeyDown)
+    {
+        return;
+    }
+
+    //printf("key pressed\n");
+
+    if (ker.wVirtualKeyCode != VK_RETURN && ker.wVirtualKeyCode != VK_ESCAPE)
+    {
+        charbuf[bufidx] = ker.uChar.AsciiChar;
+        bufidx++;
+    }
+    else if (ker.wVirtualKeyCode == VK_RETURN)
+    {
+        charbuf[bufidx] = '\0';
+        bufidx++;
+        //wprintf(wcharbuf);
+        client.Send((char*)charbuf, bufidx);
+        bufidx = 0;
+    }
+    else
+    {
+        client.Close();
+    }
+}
+
+VOID MouseEventProc(MOUSE_EVENT_RECORD mer)
+{
+#ifndef MOUSE_HWHEELED
+#define MOUSE_HWHEELED 0x0008
+#endif
+    printf("Mouse event: ");
+
+    switch (mer.dwEventFlags)
+    {
+    case 0:
+
+        if (mer.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED)
+        {
+            printf("left button press \n");
+        }
+        else if (mer.dwButtonState == RIGHTMOST_BUTTON_PRESSED)
+        {
+            printf("right button press \n");
+        }
+        else
+        {
+            printf("button press\n");
+        }
+        break;
+    case DOUBLE_CLICK:
+        printf("double click\n");
+        break;
+    case MOUSE_HWHEELED:
+        printf("horizontal mouse wheel\n");
+        break;
+    case MOUSE_MOVED:
+        printf("mouse moved\n");
+        break;
+    case MOUSE_WHEELED:
+        printf("vertical mouse wheel\n");
+        break;
+    default:
+        printf("unknown\n");
+        break;
+    }
+}
+
+VOID ResizeEventProc(WINDOW_BUFFER_SIZE_RECORD wbsr)
+{
+    printf("Resize event\n");
+    printf("Console screen buffer is %d columns by %d rows.\n", wbsr.dwSize.X, wbsr.dwSize.Y);
+}
+
 int main()
 {
     short* error = new short(-1);
@@ -29,6 +136,74 @@ int main()
 
     //len = (int)strlen(msg) + 1;
     //bytes_sent = send(sckt, msg, len, 0);
+
+    DWORD cNumRead, fdwMode, i;
+    INPUT_RECORD irInBuf[128];
+    int counter = 0;
+
+    // Get the standard input handle.
+
+    hStdin = GetStdHandle(STD_INPUT_HANDLE);
+    if (hStdin == INVALID_HANDLE_VALUE)
+        ErrorExit((LPSTR)"GetStdHandle");
+
+    // Save the current input mode, to be restored on exit.
+
+    if (!GetConsoleMode(hStdin, &fdwSaveOldMode))
+        ErrorExit((LPSTR)"GetConsoleMode");
+
+    // Enable the window and mouse input events.
+
+    fdwMode = ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT;
+    if (!SetConsoleMode(hStdin, fdwMode))
+        ErrorExit((LPSTR)"SetConsoleMode");
+
+    // Loop to read and handle the next 100 input events.
+
+    while (counter++ <= 100)
+    {
+        // Wait for the events.
+
+        if (!ReadConsoleInput(
+            hStdin,      // input buffer handle
+            irInBuf,     // buffer to read into
+            128,         // size of read buffer
+            &cNumRead)) // number of records read
+            ErrorExit((LPSTR)"ReadConsoleInput");
+
+        // Dispatch the events to the appropriate handler.
+
+        for (i = 0; i < cNumRead; i++)
+        {
+            switch (irInBuf[i].EventType)
+            {
+            case KEY_EVENT: // keyboard input
+                KeyEventProc(irInBuf[i].Event.KeyEvent, client);
+                break;
+
+            case MOUSE_EVENT: // mouse input
+                MouseEventProc(irInBuf[i].Event.MouseEvent);
+                break;
+
+            case WINDOW_BUFFER_SIZE_EVENT: // scrn buf. resizing
+                ResizeEventProc(irInBuf[i].Event.WindowBufferSizeEvent);
+                break;
+
+            case FOCUS_EVENT:  // disregard focus events
+
+            case MENU_EVENT:   // disregard menu events
+                break;
+
+            default:
+                ErrorExit((LPSTR)"Unknown event type");
+                break;
+            }
+        }
+    }
+
+    // Restore input mode on exit.
+
+    SetConsoleMode(hStdin, fdwSaveOldMode);
 
     return 0;
 }
